50aaa4eb1307feb45e3461659ec654b8a92d5bc8
Menelio <Menelioalvarez@yahoo.com>
2019-11-18 13:20:08 -0500

    11/18/2019

--- ProjectLibrary/Astar.cs
+++ ProjectLibrary/Astar.cs
@@ -7,15 +7,7 @@ using System.Threading.Tasks;
 namespace ProjectLibrary
 {
     public class Astar
-    {        /// <summary>
-             /// Astar Search recursive method
-             /// </summary>
-             /// <param name="start">Starting node</param>
-             /// <param name="goal">Goal node</param>
-             /// <param name="route">Route of nodes through the graph should start with starting node</param>
-             /// <returns></returns>
-
-        
+    { 
         /// <summary>
         /// Astar Search recursive method
         /// </summary>
@@ -24,6 +16,7 @@ namespace ProjectLibrary
         /// <param name="route">Route of nodes through the graph should start with starting node</param>
         /// <returns></returns>
         public static Node[] search(Node start, Node goal, Node[] route) {
+          
             // test for base case / stop condition
             if (start.Equals(goal)){
                 //make space in route and add cheapest node
