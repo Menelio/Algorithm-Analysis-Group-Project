0b400ebef172e253ba165a0225b94a98565c18a6
DrQ-14 <58020122+DrQ-14@users.noreply.github.com>
2019-12-01 16:38:23 -0500

    Add files via upload

---
+++ AStarSearchForm/ForTesting/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectLibrary;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ForTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Graph g1 = RandomGraph.Generate(100, 1500, 1500);
+            g1.setHnOfNodes(g1.nodes[g1.nodes.Length - 1]);
+            //warmup
+            Astar.Search(g1.nodes[0], g1.nodes[g1.nodes.Length - 1], g1);
+            Exhaustive.Search(g1.nodes[0], g1.nodes[g1.nodes.Length - 1], g1);
+
+            for (int i = 1; i < 100; i++)
+            {
+                int n = 10*i;
+                double x = 1000 * i;
+                double y = 1000 * i;
+
+                ///////////////////////////////////////////////////////////////////////////////////
+                Graph g = RandomGraph.Generate(n, x, y);
+
+                Stopwatch stopWatch1 = new Stopwatch();
+                stopWatch1.Start();
+
+                Route r1 = Astar.Search(g.nodes[0], g.nodes[g.nodes.Length - 1], g);
+                
+
+                stopWatch1.Stop();
+
+                ////////////////////////////////////////////////////////////////////////////////////
+
+                Stopwatch stopWatch2 = new Stopwatch();
+                stopWatch2.Start();
+
+                Route r2 = Exhaustive.Search(g.nodes[0], g.nodes[g.nodes.Length - 1], g);
+
+                stopWatch2.Stop();
+
+                ///////////////////////////////////////////////////////////////////////////////////
+
+                Console.WriteLine("--------------------------> "+i);
+                Console.WriteLine("A* RunTime = " + stopWatch1.ElapsedTicks);
+                Console.WriteLine("Ex RunTime = " + stopWatch2.ElapsedTicks);
+                Console.WriteLine("--------------------------");
+            }
+            //PAUSE
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ AStarSearchForm/ProjectLibrary/Astar.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectLibrary
+{
+    public class Astar
+    {
+        /// <summary>
+        /// Overloaded Wrapper method for Astar.search.
+        /// Both start and goal have to be nodes in graph
+        /// </summary>
+        /// <param name="start">Starting node</param>
+        /// <param name="goal">Goal node</param>
+        /// <param name="graph">Graph</param>
+        /// <returns>Shortest route threw graph</returns>
+        public static Route Search(Node start, Node goal, Graph graph) {
+            try
+            {
+                if (!Utils.NoRepeateNode(start, graph.nodes) && !Utils.NoRepeateNode(goal, graph.nodes))
+                {
+                    graph.setHnOfNodes(goal);
+                    Route startingRoute = new Route(start);
+                    Route[] openRoutes = new Route[0];
+                    return search(start, goal, startingRoute, openRoutes);
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+            catch (System.Exception e) {
+                Console.WriteLine("Either goal or start were not in graph, in Search(Node start,"
+                        + "Node goal, Graph graph)\n Stack trace:"+ e.StackTrace);
+                return null;
+            } 
+        }
+        
+
+
+        //Method that preforms actual A* search 
+        private static Route search(Node start, Node goal, Route currRoute, Route[] open) {
+            if (start.name.Equals(goal.name))
+            {
+                return currRoute;
+            }
+            else
+            {
+                //create new routes from nodes connected to start node and add them to open
+                for (int i = 0; i < start.edges.Length; i++)
+                {
+                    open = Utils.AppentToArray(open,  //append to open
+                        new Route(                    //a new route 
+                        Utils.AppentToArray(          //made from
+                        currRoute.nodes, start.edges[i].node), //currRoute + start.edges[i].node
+                        currRoute.totalGn + start.edges[i].gn) //add edge's gn to totalGn
+                        );
+                }
+                
+                //select cheapst route to open Last node
+                int indexOfCheapest = 0;
+                Route cheapest = open[0];
+                for (int i = 0; i < open.Length; i++)
+                {
+                    if (open[i].fn < cheapest.fn)
+                    {
+                        cheapest = open[i];
+                        indexOfCheapest = i;
+                    }
+                }
+                //close/remove cheapest route from open
+                open=Utils.RemoveAt(open, indexOfCheapest);
+                return search(cheapest.nodes[cheapest.nodes.Length - 1], goal, cheapest, open);
+
+            }
+            }
+    }
+}
+
+
+
